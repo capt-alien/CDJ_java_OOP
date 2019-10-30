fb8c94b644b4494919b1d3f03d22579bcb29d669
capt-alien <eric.botcher@presidio.edu>
2019-10-30 14:56:59 -0700

    I think I got this thing working

---
+++ ZooKeeper/src/zoo/Mammal.java
@@ -0,0 +1,13 @@
+package zoo;
+
+public class Mammal {
+	public int energyLevel;
+
+	public Mammal(int energy) {
+		this.energyLevel = energy;
+	}
+	public int displayEnergy() {
+		return this.energyLevel;
+	}
+
+}
