8ceeeb4c3f710dce2a9ed3fe1df5d31d6a3fed3c
capt-alien <eric.botcher@presidio.edu>
2019-11-02 12:38:03 -0700

    calc start

---
+++ calcProj/src/calc/Calculator.java
@@ -0,0 +1,22 @@
+package calc;
+
+public class Calculator {
+	double operandOne;
+	double operandTwo;
+	char operation;
+	double result;
+	
+	public Calculator() {
+	}
+	
+	public void setOperandOne(double operandOne){
+		this.operandOne = operandOne;	
+	}
+	
+	public void setOperation(char operernd) {
+		this.operation = operernd;
+	}
+	
+	
+//	END
+}
